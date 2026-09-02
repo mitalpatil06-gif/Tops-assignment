170b9678e4b57f7703b19e90ea664b9504b63a1b
mitalpatil06-gif <mitalpatil06@gmail.com>
2026-09-02 17:04:49 +0530

    Add files via upload

---
+++ session1.py
@@ -0,0 +1,42 @@
+#  tsak : 1 Install Anaconda on your system, open Anaconda Navigator, and launch Jupyter Notebook from it.
+
+"""
+1. Download and install Anaconda on the system.
+2. Open Anaconda Navigator.
+3. In Anaconda Navigator, find Jupyter Notebook.
+4. Click on Launch.
+5. Jupyter Notebook will open in the web browser.
+6. Create or open a Python Notebook (.ipynb) and start working.
+"""
+# task : 2 Create a new Jupyter Notebook file named analytics_intro.ipynb and write a cell that prints your name 
+#          and your favorite app (like Instagram or Zomato) using the print() function.
+"""
+1. Open Jupyter Notebook from Anaconda Navigator.
+2. Click on New → Python 3 (ipykernel) to create a new notebook.
+3. Click on File → Rename and name the file analytics_intro.ipynb.
+4. In the first cell, write:
+#  print("My name is Mital")
+#   print("My favorite app is Instagram")
+5. Run the cell using the Run button.
+6. The name and favorite app will be displayed as the output.
+"""
+
+# task : 3 Open VS Code or any code editor, create a Python script file called hello_app.py, and write code to print 'Welcome to 
+#          Data Analytics with Python!'. Then, run this script from the terminal.
+
+"""
+file name = hello_app.py 
+"""
+
+# task : 4 In Jupyter Notebook, add a new cell and write Python code to print the current year using the datetime module.<br><br><em><strong>Hint:</strong> 
+#          Import datetime and use datetime.datetime.now().year.</em>
+
+# 1. Open Jupyter Notebook.
+# 2. Create a new notebook or open an existing notebook.
+# 3. Add a new cell.
+# 4. Write the following Python code:
+"""import datetime
+
+print(datetime.datetime.now().year)"""
+# 5. Run the cell.
+# 6. The current year (2026) will be displayed as the output.
